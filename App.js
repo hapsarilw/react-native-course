c4756b8c58d1e9eebdcab61cf949ea3a6a84ca0b
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2021-11-01 19:21:15 +0800

    add inline style

--- App.js
+++ App.js
@@ -1,47 +1,31 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        padding: 50,
-        flexDirection: "row",
-        width: "80%",
-        height: 300,
-        justifyContent: "space-around",
-        alignItems: "stretch",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "red",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>1</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
+        <Button title="ADD" />
       </View>
-      <View
-        style={{
-          backgroundColor: "blue",
-          flex: 2,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: "green",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>3</Text>
-      </View>
+      <View style={styles.inputContainer}/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }  
+});
