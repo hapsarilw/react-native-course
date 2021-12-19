3b313b9300cf2e2510a933205478fb893b3bb5eb
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2021-12-19 22:13:53 +0800

    adding filtering logic

--- store/actions/meals.js
+++ store/actions/meals.js
@@ -3,3 +3,7 @@ export const TOGGLE_FAVORITE = 'TOGGLE_FAVORITE';
 export const toggleFavorite = (id) => {
     return { type: TOGGLE_FAVORITE, mealId: id };
 };
+
+export const setFilter = filterSettings => {
+    return { type: SET_FILTERS, filters: filterSettings }
+};
