50a39a4985b8c9c79801d12209dc4865e58c74c1
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2021-12-20 16:13:55 +0800

    dispatching redux in react native apps

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,23 +1,30 @@
 import React from "react";
-import { useSelector } from 'react-redux';
+import { View, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
 
 import { CATEGORIES } from "../data/dummy-data";
 import MealList from "../components/MealList";
+import DefaultText from "../components/DefaultText";
 
 const CategoryMealsScreen = (props) => {
-  
   const catId = props.navigation.getParam("categoryId");
   // derive data & will return data from global state
-  const availableMeals = useSelector(state => state.meals.filteredMeals); //retrieve data out of state & return it.
+  const availableMeals = useSelector((state) => state.meals.filteredMeals); //retrieve data out of state & return it.
 
   // Find the meals that belong to that category selected
   const displayedMeals = availableMeals.filter(
     (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
 
-  return (
-   <MealList listData={displayedMeals} navigation={props.navigation}/>
-  );
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found, maybe check your filters?</DefaultText>
+      </View>
+    );
+  }
+
+  return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 
 CategoryMealsScreen.navigationOptions = (navigationData) => {
@@ -29,5 +36,13 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedCategory.title,
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default CategoryMealsScreen;
