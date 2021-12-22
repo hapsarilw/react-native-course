bbf3896968662e6fc095359aa8f86ec6a8259d36
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2021-12-22 22:28:32 +0800

    setting up a navigator

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Text, View } from "react-native";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
 import productReducer from "./store/reducers/products";
+import ShopNavigator from "./navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
-  products: productReducer,
+  products: productReducer // product slice
 });
 
 const store = createStore(rootReducer);
@@ -14,7 +14,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-      <View>...</View>
+      <ShopNavigator />
     </Provider>
   );
 }
