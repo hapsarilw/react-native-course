a5663f18448e7de7d5ced031246612fd24028617
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2022-01-08 10:25:00 +0800

    Using Params to Submit User Input

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import {
   View,
   ScrollView,
@@ -27,8 +27,13 @@ const EditProductScreen = (props) => {
     editedProduct ? editedProduct.description : ""
   );
 
-  if (prodId) {
-  }
+  const submitHandler = useCallback(() => {
+    console.log('Submitting!');
+  }, []);
+
+  useEffect(() => {
+    props.navigation.setParams({ submit: submitHandler });
+  }, [submitHandler]);
 
   return (
     <ScrollView>
@@ -73,6 +78,7 @@ const EditProductScreen = (props) => {
 };
 
 EditProductScreen.navigationOptions = (navData) => {
+  const submitFn = navData.navigation.getParam('submit');
   return {
     headerTitle: () => {
       navData.navigation.getParam("productId") ? "Edit Product" : "Add Product";
@@ -84,7 +90,7 @@ EditProductScreen.navigationOptions = (navData) => {
           iconName={
             Platform.OS === "android" ? "md-checkmark" : "ios-checkmark"
           }
-          onPress={() => {}}
+          onPress={submitFn}
         />
       </HeaderButtons>
     ),
