8534fd098aa413f03edb342b88a03a7e02d10d4c
Hapsari Laksmi Wijayanti <hapsari.laksmiw@gmail.com>
2022-02-06 11:56:28 +0800

    Storing Data in the Local Database

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,21 +1,33 @@
 import * as FileSystem from "expo-file-system";
+
 export const ADD_PLACE = "ADD_PLACE";
+import { insertPlace } from "../helpers/db";
 
 export const addPlace = (title, image) => {
   return async (dispatch) => {
-    const fileName = image.split('/').pop();
+    const fileName = image.split("/").pop();
     const newPath = FileSystem.documentDirectory + fileName;
     // using try catch because it will fail -> permission error / not enough space
     try {
       await FileSystem.moveAsync({
         from: image,
-        to: newPath
+        to: newPath,
+      });
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        "Dummy address",
+        15.6,
+        12.3
+      );
+      console.log(dbResult);
+      dispatch({
+        type: ADD_PLACE,
+        placeData: { id: dbResult.insertId, title: title, image: newPath },
       });
     } catch (err) {
       console.log(err);
       throw err;
-    }    
-
-    dispatch ({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
+    }
   };
 };
